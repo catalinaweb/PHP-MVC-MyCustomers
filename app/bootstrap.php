2a1096fe144f85e4251e364cc0effa646ba659c4
catalinaweb <35579554+catalinaweb@users.noreply.github.com>
2020-10-17 10:28:02 +0300

    Update bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,12 @@
 
+<?php
+//Config
+require_once '../app/config/config.php';
+
+//Load helpers
+require_once 'helpers/redirect_helper.php';
+
+//Autoload core libraries
+spl_autoload_register(function($className){
+    require_once 'libraries/'.$className.'.php';
+});
