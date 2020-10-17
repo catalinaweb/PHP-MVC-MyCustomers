68881a3abf868ba5d3b2688236d7c2fde162a5c5
catalinaweb <35579554+catalinaweb@users.noreply.github.com>
2020-10-17 10:52:11 +0300

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DB_HOST','localhost');
+define('DB_USER','scanar41_mycust');
+define('DB_PASS','5A)yPJ9+?Lhe');
+define('DB_NAME','scanar41_mycustomers');
+
+
+//aPP ROOT
+define('APPROOT',dirname(dirname(__FILE__)));
+
+//URL ROOT
+define('URLROOT', 'http://mycustomers.topwebdeveloper.co.uk/');
+
+
